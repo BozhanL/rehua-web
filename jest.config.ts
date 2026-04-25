22e84aef3c93574583d62c40514c5150c0224400
Bozhan Liang <lbz@rivensbane.com>
2026-04-25 04:58:09 +0000

    Enable typia

--- jest.config.ts
+++ jest.config.ts
@@ -182,7 +182,14 @@ const config: Config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx|mjs)$": [
+      "ts-jest",
+      {
+        tsconfig: "./tsconfig.jest.json",
+      },
+    ],
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-import { env } from "node:process";
-
-const nextConfig: NextConfig = {
-  basePath: env["BASE_URL"] ?? "",
-  output: "export",
-};
-
-export default nextConfig;
